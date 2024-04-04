8038ea1005254b6340f28083dd192b526e3bfd3f
Laura Boetcher <g33klaura@gmail.com>
2024-04-03 23:45:51 -0600

    Sturdy foundations

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwECU2eBkvnWZMxFwWOPLn0azdwaa3tw4",
+  authDomain: "birb-track-db.firebaseapp.com",
+  databaseURL: "https://birb-track-db-default-rtdb.firebaseio.com",
+  projectId: "birb-track-db",
+  storageBucket: "birb-track-db.appspot.com",
+  messagingSenderId: "1015955307094",
+  appId: "1:1015955307094:web:9f8c8e6007c8d1214a19f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
